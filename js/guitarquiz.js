6a2208cce111f0d81ea4e015fc9d86b92d8f1525
nuhuxi <mark.keckeis@gmail.com>
2014-04-25 13:45:22 -0700

    140425 1325

--- js/guitarquiz.js
+++ js/guitarquiz.js
@@ -57,14 +57,20 @@ $(document).ready(function () {
   $('.intro').click(function () {
   		$('#intro').fadeIn(1000);
   });
-
+  $('#howDidYouDo').click(function () {
+    console.log("Summary clicked!");
+    playCliffsOfDover();
+    $('#answer').slideUp(800, function(){
+      $('#summary').slideDown(800);
+      $('#answerCanBeSwappedOut').remove();
+    });
+  });
 
 
   $('.quizAnswer').click(function(){
       theAnswerIs = this.id; 
       /*-- Check to see if answer is correct. --*/
       checkAnswer();
-
       currentUnit = '#question' + questionNumber; 
   		$(currentUnit).slideUp(800, function(){
         loadAnswer();
@@ -76,18 +82,30 @@ $(document).ready(function () {
   });
 
   $('#nextQuestion').click(function(){
-    console.log('In nextQuestion. questionNumbre is '+ questionNumber);
+
     questionNumber ++;
     loadQuestion(questionNumber);
     currentUnit = '#question' + questionNumber; 
-  	$('#answer').slideUp(800, function(){
-  		$(currentUnit).slideDown(800);
-      $('#answerCanBeSwappedOut').remove();
-  	});
-  });
+    console.log('question number is '+questionNumber);
+    console.log('quizUnits.length is')
+    if (questionNumber < quizUnits.length-1) {
+       $('#answer').slideUp(800, function(){
+          $(currentUnit).slideDown(800);
+          $('#answerCanBeSwappedOut').remove();
+         })
+    }
+    else{
+        playCliffsOfDover();
+        $('#answer').slideUp(800, function(){
+          $('#summary').slideDown(800);
+          $('#answerCanBeSwappedOut').remove();
+          });
+      }
+    });
+         
+  
 
   $('.new').click(function(){
-
       questionNumber = 0;
       numRightAnswers = 0;
       $('.question').remove();
@@ -114,21 +132,19 @@ $(document).ready(function () {
 
 	/*--- User wants a new game. ---*/
   	$('.new').click(function(){
-
       questionNumber = 0;
       numRightAnswers = 0;
       $('.question').remove();
       $('#answerCanBeSwappedOut').remove();
       hideAll();
       $("#intro").fadeIn(1000);
-
       loadQuestion(questionNumber);
       $('#question0').show();
     });
 
     function resetGame (){
       questionNumber = 0;
-      /*numGuesses = 0; */
+
       numRightAnswers = 0;
 
       loadArray();
@@ -145,11 +161,14 @@ $(document).ready(function () {
       $("#question2").hide();
       $("#question3").hide();
       $("#question4").hide();
+      $('#summary').hide();
+      $('#holdingPlace').hide();
 
   		$("#answer").hide();
     };
 
     function loadQuestion(){
+      //playDriveMyCar();
 
       currentQuestion = quizUnits[questionNumber];
       markerNumber = '#marker' + questionNumber;
@@ -190,7 +209,7 @@ $(document).ready(function () {
       /*-- If the question were loaded in a CMS this would be the place where we would load questions into the array --*/
       quizUnits[0] = new QuestionAnswer(
         'Inspired by Chet Atkins, this guitar saw a sales spike after The Beatles appearance on the Ed Sullivan Show.',
-        'The Gretsch Musical Instrument Company built the first "Country Gentleman" guitar for Chet Atkins in 1957. When George Harrison played it on the Ed Sullivan Show seeing sales spike by 25% in one week.',
+        'The Gretsch Musical Instrument Company built the first "Country Gentleman" guitar for Chet Atkins in 1957. When George Harrison played it on the Ed Sullivan Show, sales spiked by 25% in one week.',
         'gretsch',
         'geoharrison',
         'gretsch-guitar');
@@ -199,28 +218,28 @@ $(document).ready(function () {
       /*-- Next question --*/
       quizUnits[1] = new QuestionAnswer(
         'Perhaps the most famous electric guitar of the rock and roll era, the Stratocaster - or Strat - was the the choice of Jimmy Hendrix and living legends Eric Clapton, Jeff Beck, and John Mayer. It was designed by a man who never learned to play the guitar. <br/><br/>What was his last name?',
-        'Leo Fender started the Fender Musical Instrument Company in the depths of the Great Depression. His first guitar, the Broadcaster later became the Telecaster which is still by artists such as Bruce Springsteen and Brad Paisley. Leo also manufactured the first electric bass guitar.',
+        'Leo Fender started the Fender Musical Instrument Company in the depths of the Great Depression. His first guitar, the Broadcaster later became the Telecaster which is still a country music standard and played by artists such as Bruce Springsteen and Brad Paisley. Leo also manufactured the first electric bass guitar.',
         'fender',
         'claptonplaying',
         'fender-strat-hardtail');
 
       /*-- Next question --*/
       quizUnits[2] = new QuestionAnswer(
-        'Innovative guitarist Les Paul designed the guitar that bears his name. He was a pioneer with his partner Mary Ford in the use of magnestic tape for multi-track recording. The Les Paul shot to stardom in the hands Jimmy Page and is still found on rock stages the world over. <br/><br/>What company manufactured it?',
+        'Innovative guitarist Les Paul designed the guitar that bears his name. He and his partner Mary Ford, were pioneers in the use of magnetic tape for multi-track recording. The Les Paul shot to stardom in the hands Jimmy Page and is still found on rock stages the world over. <br/><br/>What company manufactured it?',
         'In 1902, Orville Gibson started the Gibson Guitar-Mandolin Mfg Co. Ltd. in Kalamazoo MI. Over the years Gibson made everything from mandolins to aircraft parts during WWII. The Les Paul was probably their most commercially successful guitar. Gibson Mandolins are prized today for their beauty and tone.',
         'gibson',
         'pageplaying',
         'les-paul-classic');
 
       quizUnits[3] = new QuestionAnswer(
-        'Eddie Van Halen rocked the music industry in the early 80s with his astounding guitar technique. He could not have done it without the innovation of the EVH guitar. A locking "nut" made it possible to use the "whammy bar" to bend notes like no one had done before. Oh, he had great hair too.<br/><br/> Who manufactured his guitar?',
-        'The Kramer Guitar Company was founded in the late 1970s by Dennis Berardi, Gary Kramer and Phillip J. Petillo Ph.D., Master Luthier-Engineer to manufacture aluminum-necked guitars. Based in New Jersey, they produced guitars used by heavy metal and shred guitar bands in the 80s',
+        'Eddie Van Halen rocked the music industry in the early 80&#146s with his astounding guitar technique. He could not have done it without the innovation of the EVH guitar. A locking "nut" made it possible to use the "whammy bar" to bend notes like no one had done before. Oh, he had great hair too.<br/><br/> Who manufactured his guitar?',
+        'The Kramer Guitar Company was founded in the late 1970s by Dennis Berardi, Gary Kramer and Phillip J. Petillo Ph.D., Master Luthier-Engineer to manufacture aluminum-necked guitars. Based in New Jersey, they produced guitars used by heavy metal and shred guitar bands in the 80s.<br/> <br/> The guitar solo in Michael Jackson&#146s "Beat It" was played by Eddie VanHalen.',
         'kramer',
         'evhplaying',
         'kramer-guitar');
 
       quizUnits[4] = new QuestionAnswer(
-        'Almost single-handedly, Carlos Santana brought this guitar company to the fore. Its design was unique with 24 frets rather that the standard 22 frets found on Fender and Gibson guitars. The addition of 2 frets changed the harmonics of the instrument allowing Carlos and many others to sing with their guitars. <br/><br/> Who manufactured his guitar?',
+        'Almost single-handedly, Carlos Santana brought this guitar company to the fore. It&#146s design was unique with 24 frets rather that the standard 22 frets found on Fender and Gibson guitars. The addition of 2 frets changed the harmonics of the instrument allowing Carlos and many others to sing with their guitars. <br/><br/> Who manufactured his guitar?',
         'Paul Reed Smith(PRS) was a Maryland based guitar player who pioneered the use of CNC machines to produce electric guitars of the highest quality. PRS now produces approximately 700 guitars a month from their factory in Stevensville, MD. <br/>Not bad for a kid who started making guitars in college.',
         'prs',
         'carlos',
@@ -238,8 +257,70 @@ $(document).ready(function () {
       var checkTheAnswer = quizUnits[questionNumber];
       if(theAnswerIs===checkTheAnswer.rightAnswer){
         numRightAnswers++;
+        switch(questionNumber){
+          case 0:
+            playDriveMyCar();
+            break;
+          case 1:
+            playLayla();
+            break;
+          case 2:
+            playCommunicationBreakdown();
+            break;
+          case 3:
+            console.log('The Question number is '+questionNumber)
+            console.log('play Beat It!');
+            playBeatIt();
+            break;
+          case 4:
+            console.log('Play Santana!');
+            playSantana();
+            break;
+          }
+        }
+        else{
+            console.log('Play Hayseed!')
+            playHayseed();
+        }
       };
+    //};
+
+    function playDriveMyCar() {
+      $('#sound-drivemycar')[0].volume = 0.5;
+      $('#sound-drivemycar')[0].load();
+      $('#sound-drivemycar')[0].play();
     };
+    function playLayla() {
+      $('#sound-layla')[0].volume = 0.5;
+      $('#sound-layla')[0].load();
+      $('#sound-layla')[0].play();
+    };
+    function playCommunicationBreakdown() {
+      $('#sound-communicationbreakdown')[0].volume = 0.5;
+      $('#sound-communicationbreakdown')[0].load();
+      $('#sound-communicationbreakdown')[0].play();
+    };
+    function playBeatIt() {
+      $('#sound-beatit')[0].volume = 0.5;
+      $('#sound-beatit')[0].load();
+      $('#sound-beatit')[0].play();
+    };
+    function playSantana() {
+      $('#sound-santana')[0].volume = 0.5;
+      $('#sound-santana')[0].load();
+      $('#sound-santana')[0].play();
+    };
+    function playHayseed() {
+      $('#sound-hayseed')[0].volume = 0.5;
+      $('#sound-hayseed')[0].load();
+      $('#sound-hayseed')[0].play();
+    };
+    function playCliffsOfDover() {
+      $('#sound-cliffsofdover')[0].volume = 0.5;
+      $('#sound-cliffsofdover')[0].load();
+      $('#sound-cliffsofdover')[0].play();
+    };
+    //}
 
 	event.preventDefault();  	
 });
